64c5a756dac30ef963377c3629181e6df5d27799
Akshay Vishwanaath Vallioor Ramachandran <32500748+akshayvvr@users.noreply.github.com>
2019-10-07 11:42:08 -0700

    Create GitPull.py

---
+++ GitPull/GitPull.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import glob
+from subprocess import Popen
+
+def listdir_nohidden(path):
+    return glob.glob(os.path.join(path, '*'))
+
+listdir=listdir_nohidden(os.curdir)
+print("\nFiltering only Directories in the Current Folder")
+directories_in_curdir = filter(os.path.isdir, listdir)
+# print(directories_in_curdir)
+
+for number,repo in enumerate(sorted(directories_in_curdir)):
+    try:
+        print(f'\n\nPulling from {repo}')
+        git_pull=Popen(f'git pull origin master',shell=True,cwd=f'./{repo}').wait()
+    
+    except Exception as e:
+        print(f'Exception Found at Repo {repo}')
+        print(e)
+
+print(f'All Done')
